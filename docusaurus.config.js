fb76d4e1473adddf017123684c3a29231f76c725
Eric Tuvesson <eric.tuvesson@gmail.com>
2025-10-28 16:44:37 +0100

    feat: Upgrade packages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,44 +1,46 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const {
-  _parseNoodlMarkupPlugin,
-  _parseRenderedMarkupPlugin,
-} = require('./plugins/markdown-syntax');
+import {
+  customMarkupPlugin,
+  customRenderedMarkupPlugin,
+} from "./plugins/markdown-syntax";
+import { includeMarkdownPlugin } from "./plugins/import-markdown";
+import { copyNodeMarkdownPlugin } from "./plugins/copy-node-markdowns";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Fluxscape Docs',
-  tagline: 'Fluxscape Documentation',
-  url: 'https://docs.fluxscape.io',
+  title: "Fluxscape Docs",
+  tagline: "Fluxscape Documentation",
+  url: "https://docs.fluxscape.io",
   baseUrl: `/`,
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'Fluxscape', // Usually your GitHub org/user name.
-  projectName: 'Fluxscape', // Usually your repo name.
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "Fluxscape", // Usually your GitHub org/user name.
+  projectName: "Fluxscape", // Usually your repo name.
 
   // This depends on the hosting provider
   // trailingSlash: true,
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           breadcrumbs: false,
         },
         theme: {
           customCss: [
-            require.resolve('./src/css/custom.css'),
-            require.resolve('./src/css/variables.css'),
-            require.resolve('./src/css/navbar.scss'),
-            require.resolve('./src/css/sidebar.scss'),
-            require.resolve('./src/css/searchbar.scss'),
-            require.resolve('./src/css/markdown.scss'),
-            require.resolve('./src/css/pagination.scss'),
+            require.resolve("./src/css/custom.css"),
+            require.resolve("./src/css/variables.css"),
+            require.resolve("./src/css/navbar.scss"),
+            require.resolve("./src/css/sidebar.scss"),
+            require.resolve("./src/css/searchbar.scss"),
+            require.resolve("./src/css/markdown.scss"),
+            require.resolve("./src/css/pagination.scss"),
           ],
         },
       }),
@@ -46,87 +48,78 @@ const config = {
   ],
 
   plugins: [
-    'docusaurus-plugin-sass',
+    "docusaurus-plugin-sass",
 
     // https://github.com/praveenn77/docusaurus-lunr-search
     [
-      '@easyops-cn/docusaurus-search-local',
+      "@easyops-cn/docusaurus-search-local",
       /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
       {
         hashed: true,
-        searchBarPosition: 'left',
+        searchBarPosition: "left",
       },
     ],
 
     // Node reference docs
     [
-      '@docusaurus/plugin-content-docs',
+      "@docusaurus/plugin-content-docs",
       {
-        id: 'nodes',
-        path: 'nodes',
-        routeBasePath: 'nodes',
-        sidebarPath: require.resolve('./sidebarsNodes.js'),
+        id: "nodes",
+        path: "nodes",
+        routeBasePath: "nodes",
+        sidebarPath: require.resolve("./sidebarsNodes.js"),
         breadcrumbs: false,
-        remarkPlugins: [
-          _parseNoodlMarkupPlugin,
-          require('./plugins/import-markdown'),
-        ],
-        rehypePlugins: [_parseRenderedMarkupPlugin],
+        remarkPlugins: [customMarkupPlugin, includeMarkdownPlugin],
+        rehypePlugins: [customRenderedMarkupPlugin],
       },
     ],
     // Javascript API
     [
-      '@docusaurus/plugin-content-docs',
+      "@docusaurus/plugin-content-docs",
       {
-        id: 'javascript',
-        path: 'javascript',
+        id: "javascript",
+        path: "javascript",
         breadcrumbs: false,
-        routeBasePath: 'javascript',
-        sidebarPath: require.resolve('./sidebarsJavascript.js'),
+        routeBasePath: "javascript",
+        sidebarPath: require.resolve("./sidebarsJavascript.js"),
       },
     ],
 
     // Library
     [
-      '@docusaurus/plugin-content-docs',
+      "@docusaurus/plugin-content-docs",
       {
-        id: 'library',
-        path: 'library',
-        routeBasePath: 'library',
+        id: "library",
+        path: "library",
+        routeBasePath: "library",
         breadcrumbs: false,
-        sidebarPath: require.resolve('./sidebarsLibrary.js'),
-        remarkPlugins: [
-          _parseNoodlMarkupPlugin,
-          require('./plugins/import-markdown'),
-        ],
+        sidebarPath: require.resolve("./sidebarsLibrary.js"),
+        remarkPlugins: [customMarkupPlugin, includeMarkdownPlugin],
       },
     ],
 
     // Whats new
     [
-      '@docusaurus/plugin-content-blog',
+      "@docusaurus/plugin-content-blog",
       {
-        id: 'whats-new',
-        path: 'whats-new',
-        routeBasePath: 'whats-new',
-        blogSidebarTitle: 'Recent updates',
+        id: "whats-new",
+        path: "whats-new",
+        routeBasePath: "whats-new",
+        blogSidebarTitle: "Recent updates",
         postsPerPage: 1,
-        feedOptions: { type: 'json' },
+        feedOptions: { type: "json" },
         showReadingTime: false,
-        remarkPlugins: [
-          _parseNoodlMarkupPlugin,
-          require('./plugins/import-markdown'),
-        ],
+        remarkPlugins: [customMarkupPlugin, includeMarkdownPlugin],
       },
     ],
 
     // Copy static md files for editor inline docs
     [
-      require('./plugins/copy-node-markdowns'),
+      copyNodeMarkdownPlugin,
       {
         paths: [
-          { path: 'nodes/', folderName: true },
-          { path: 'library/modules/' },
+          { path: "nodes/", folderName: true },
+          { path: "library/modules/" },
         ],
       },
     ],
@@ -135,72 +128,72 @@ const config = {
   themeConfig: {
     metadata: [
       {
-        property: 'og:image',
-        content: 'https://docs.fluxscape.io/img/fluxscape-docs-banner.jpg',
+        property: "og:image",
+        content: "https://docs.fluxscape.io/img/fluxscape-docs-banner.jpg",
       },
       {
-        property: 'og:title',
-        content: 'Fluxscape Documentation',
+        property: "og:title",
+        content: "Fluxscape Documentation",
       },
       {
-        property: 'og:description',
+        property: "og:description",
         content:
-          'Explore Fluxscape guides, tutorials, videos, modules, and reference documentation here. Fluxscape is the low-code platform for designers + developers to build custom web apps and experiences.',
+          "Explore Fluxscape guides, tutorials, videos, modules, and reference documentation here. Fluxscape is the low-code platform for designers + developers to build custom web apps and experiences.",
       },
       {
-        name: 'google-site-verification',
-        content: 'KjANYZkN8ymGFD0SGnAVlRkD85p-fpNHCRAOKxurpWI',
+        name: "google-site-verification",
+        content: "KjANYZkN8ymGFD0SGnAVlRkD85p-fpNHCRAOKxurpWI",
       },
     ],
     colorMode: {
       disableSwitch: true,
-      defaultMode: 'dark',
+      defaultMode: "dark",
     },
     navbar: {
-      title: 'Docs',
+      title: "Docs",
       logo: {
-        alt: 'Fluxscape Logo',
-        src: 'img/logo.png',
+        alt: "Fluxscape Logo",
+        src: "img/logo.png",
       },
       items: [
         {
-          label: 'Learn',
-          to: '/docs/learn',
-          position: 'right',
+          label: "Learn",
+          to: "/docs/learn",
+          position: "right",
         },
         {
-          label: 'Node reference',
-          to: '/nodes/overview',
-          position: 'right',
+          label: "Node reference",
+          to: "/nodes/overview",
+          position: "right",
         },
         {
-          label: 'Library',
-          to: '/library/overview',
-          position: 'right',
+          label: "Library",
+          to: "/library/overview",
+          position: "right",
         },
         {
-          label: 'Javascript',
-          to: '/javascript/overview',
-          position: 'right',
-          className: 'has-divider',
+          label: "Javascript",
+          to: "/javascript/overview",
+          position: "right",
+          className: "has-divider",
         },
         {
-          label: 'Discord',
-          to: 'https://discord.gg/fXNW9EXa6A',
-          target: '_blank',
-          position: 'right',
-          className: 'is-discord',
+          label: "Discord",
+          to: "https://discord.gg/fXNW9EXa6A",
+          target: "_blank",
+          position: "right",
+          className: "is-discord",
         },
         {
-          label: 'Download',
-          to: 'https://github.com/fluxscape/fluxscape/releases',
-          target: '_blank',
-          position: 'right',
-          className: 'is-download-button',
+          label: "Download",
+          to: "https://github.com/fluxscape/fluxscape/releases",
+          target: "_blank",
+          position: "right",
+          className: "is-download-button",
         },
       ],
     },
   },
 };
 
-module.exports = config;
+export default config;
--- plugins/copy-node-markdowns.js
+++ plugins/copy-node-markdowns.js
@@ -1,64 +1,69 @@
-const fs = require('fs')
-const path = require('path')
+import fs from "node:fs";
+import path from "node:path";
 
 function resolveImports(content, dir) {
-    const includeMatch = content.matchAll(/@include\s\"(.*)\"/g)
-    for (const _s of includeMatch) {
-        const includePath = _s[1];
-        const absPath = path.join(dir, includePath)
+  const includeMatch = content.matchAll(/@include\s\"(.*)\"/g);
+  for (const _s of includeMatch) {
+    const includePath = _s[1];
+    const absPath = path.join(dir, includePath);
 
-        const include = fs.readFileSync(absPath)
-        content = content.replace(_s[0], include)
-    }
+    const include = fs.readFileSync(absPath);
+    content = content.replace(_s[0], include);
+  }
 
-    return content
+  return content;
 }
 
 function copyNodeMarkdowns(dir, asFolderName) {
-    fs.readdirSync(dir).forEach(function (file) {
-        if (fs.lstatSync(dir + '/' + file).isDirectory()) {
-            copyNodeMarkdowns(dir + '/' + file, asFolderName)
-        } else if (file.endsWith('.md')) {
-            const filePath = path.join(dir, file);
-            const content = fs.readFileSync(filePath)
-            const resolved = resolveImports(content.toString(), dir)
-            
-            let outputFilePath = 'build/' + filePath;
-            if (asFolderName) {
-                // HACK: Resolve the new nodes folder structure
-                outputFilePath = 'build/' + filePath.split(/\\|\//).slice(0, -1).join("/") + '.md'
-            }
+  fs.readdirSync(dir).forEach(function (file) {
+    if (fs.lstatSync(dir + "/" + file).isDirectory()) {
+      copyNodeMarkdowns(dir + "/" + file, asFolderName);
+    } else if (file.endsWith(".md")) {
+      const filePath = path.join(dir, file);
+      const content = fs.readFileSync(filePath);
+      const resolved = resolveImports(content.toString(), dir);
 
-            if (!fs.existsSync('build/' + dir)) {
-                fs.mkdirSync('build/' + dir);
-            }
-            fs.writeFileSync(outputFilePath, resolved)
-        }
-    })
+      let outputFilePath = "build/" + filePath;
+      if (asFolderName) {
+        // HACK: Resolve the new nodes folder structure
+        outputFilePath =
+          "build/" + filePath.split(/\\|\//).slice(0, -1).join("/") + ".md";
+      }
+
+      if (!fs.existsSync("build/" + dir)) {
+        fs.mkdirSync("build/" + dir);
+      }
+      fs.writeFileSync(outputFilePath, resolved);
+    }
+  });
 }
 
-module.exports = function (context, options) {
-    return {
-        name: 'docusaurus-copy-node-markdowns-plugin',
-        configureWebpack(config, isServer, utils) {
-            const {
-                getJSLoader
-            } = utils;
-            return isServer ? {
-                plugins: [{
-                    apply: (compiler) => {
-                        compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
-                            console.log('Copying node markdown files')
+export function copyNodeMarkdownPlugin(context, options) {
+  return {
+    name: "docusaurus-copy-node-markdowns-plugin",
+    configureWebpack(config, isServer, utils) {
+      const { getJSLoader } = utils;
+      return isServer
+        ? {
+            plugins: [
+              {
+                apply: (compiler) => {
+                  compiler.hooks.afterEmit.tap(
+                    "AfterEmitPlugin",
+                    (compilation) => {
+                      console.log("Copying node markdown files");
 
-                            options.paths.forEach(path => {
-                                console.log(' - Processing path: ' + path.path)
-                                copyNodeMarkdowns(path.path, !!path.folderName)
-                            })
-
-                        })
+                      options.paths.forEach((path) => {
+                        console.log(" - Processing path: " + path.path);
+                        copyNodeMarkdowns(path.path, !!path.folderName);
+                      });
                     }
-                }]
-            } : {}
-        },
-    };
-};
+                  );
+                },
+              },
+            ],
+          }
+        : {};
+    },
+  };
+}
